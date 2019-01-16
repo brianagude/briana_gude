789b97d1210ee533d05d6da52c07f9e8055a3838
Briana Gude <briana.gude@gmail.com>
2019-01-16 13:44:58 -0500

    removed about & products pages & some styling

--- src/js/global.js
+++ src/js/global.js
@@ -1,39 +1,42 @@
 //  text effect
 
-'use strict'
+"use strict";
 
-function _toArray(arr) { return Array.isArray(arr) ? arr : Array.from(arr) }
-
-function makeSpans (selector) {
-  var _document$querySelect = document.querySelectorAll(selector)
-  var _document$querySelect2 = _toArray(_document$querySelect)
-  var elements = _document$querySelect2.slice(0)
-
-  return elements.map(function (element) {
-    var text = element.innerText.split('')
-    var spans = text.map(function (letter) {
-      return '<span>' + letter + '</span>'
-    }).join('')
-    return element.innerHTML = spans
-  })
+function _toArray(arr) {
+  return Array.isArray(arr) ? arr : Array.from(arr);
 }
 
-makeSpans('p, h1, h2, h3, h4, h5')
+function makeSpans(selector) {
+  var _document$querySelect = document.querySelectorAll(selector);
+  var _document$querySelect2 = _toArray(_document$querySelect);
+  var elements = _document$querySelect2.slice(0);
 
+  return elements.map(function(element) {
+    var text = element.innerText.split("");
+    var spans = text
+      .map(function(letter) {
+        return "<span>" + letter + "</span>";
+      })
+      .join("");
+    return (element.innerHTML = spans);
+  });
+}
+
+makeSpans("p, h1, h2, h3, h4, h5");
 
 // change elements on scroll
-document.addEventListener('scroll', function() {
-  const pixels = window.pageYOffset
-  const sections = document.querySelectorAll('section.projects')
-  const topic = document.querySelector('div.topic h4')
-  const title = document.querySelector('div.title h4')
-  const desc = document.querySelector('div.desc')
+document.addEventListener("scroll", function() {
+  const pixels = window.pageYOffset;
+  const sections = document.querySelectorAll("section.projects");
+  const topic = document.querySelector("div.topic h4");
+  const title = document.querySelector("div.title h4");
+  const desc = document.querySelector("div.desc");
 
   sections.forEach(section => {
-    if (section.offsetTop - 200 <= pixels){
-      topic.innerHTML = section.getAttribute('data-topic')
-      title.innerHTML = section.getAttribute('data-title')
-      desc.innerHTML = section.getAttribute('data-desc')
+    if (section.offsetTop - 200 <= pixels) {
+      topic.innerHTML = section.getAttribute("data-topic");
+      title.innerHTML = section.getAttribute("data-title");
+      desc.innerHTML = section.getAttribute("data-desc");
     }
-  })
-})
+  });
+});
