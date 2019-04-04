ab75d11ba75e758e9889e889d7af197cf442ebc4
Briana Gude <briana.gude@gmail.com>
2019-04-04 07:42:07 -0400

    merged both portfolio pages

---
+++ dist/js/texteffect.js
@@ -0,0 +1,12 @@
+  const makeSpans = selector => {
+    const [...elements] = document.querySelectorAll(selector)
+    return elements.map(element => {
+      const text = element.innerText.split('')
+      const spans = text
+        .map(letter => '<span>' + letter + '</span>')
+        .join('')
+      return element.innerHTML = spans
+    })
+  }
+
+  makeSpans('p, h1, h2')
