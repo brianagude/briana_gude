ef13a632be5dee7a53c13a227e0156598855c1e2
Briana Gude <briana.gude@gmail.com>
2020-01-08 22:36:01 -0500

    adding paintbox

---
+++ src/js/paintbox.js
@@ -0,0 +1,8 @@
+const canvas = document.querySelector('canvas.paintbox');
+const context = canvas.getContext('2d');
+
+canvas.height = window.innerHeight
+canvas.width = window.innerWidth
+
+context.fillStyle = 
+
